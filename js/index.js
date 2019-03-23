ed80e4fdff4067d9f080c27424193cec3ffb15a2
MichalAW <michal.wanta@gmail.com>
2019-03-23 11:58:02 +0100

    fix car and if

--- js/index.js
+++ js/index.js
@@ -1,33 +1,6 @@
-// var url = 'http://api.icndb.com/jokes/random';
-// var button = document.getElementById('get-joke');
-// button.addEventListener('click', function(){
-//   getJoke();
-// });
-// var paragraph = document.getElementById('joke');
-
-// function getJoke() {
-//   var xhr = new XMLHttpRequest();
-//   xhr.open('GET', url);
-//   xhr.addEventListener('load', function(){
-//     var response = JSON.parse(xhr.response);
-//         paragraph.innerHTML = response.value.joke;
-//   });
-//   xhr.send();
-// }
-
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
-var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
 var prefix = "https://cors-anywhere.herokuapp.com/";
-
-if (tweetText.length > 140) {
-    getQuote();
-} else {
-    var tweet = tweetLink + encodeURIComponent(tweetText);
-    document.querySelector('.quote').innerText = quoteText;
-    document.querySelector('.author').innerText = "Author: " + quoteAuthor;
-    document.querySelector('.tweet').setAttribute('href', tweet);
-}
 
 function getQuote() {
 	fetch(prefix + quoteUrl, { cache: "no-store" })
@@ -46,6 +19,16 @@ function createTweet(input) {
     dataElement.innerHTML = data.content;
     var quoteText = dataElement.innerText.trim();
     var quoteAuthor = data.title;
+    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+    if (tweetText.length > 140) {
+    getQuote();
+	} else {
+	    var tweet = tweetLink + encodeURIComponent(tweetText);
+	    document.querySelector('.quote').innerText = quoteText;
+	    document.querySelector('.author').innerText = "Author: " + quoteAuthor;
+	    document.querySelector('.tweet').setAttribute('href', tweet);
+	}
 
     if (!quoteAuthor.length) {
         quoteAuthor = "Unknown author";
