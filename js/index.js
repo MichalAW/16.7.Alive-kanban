ac6f8ebf766dc60daa9cc321a612a164c296179c
MichalAW <michal.wanta@gmail.com>
2019-04-12 00:05:18 +0200

    f

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 var url = 'https://api.icndb.com/jokes/random';
 var button = document.getElementById('get-joke');
-button.addEventListener('click', function(){
+button.addEventListener('click', function() {
   getJoke();
 });
 var paragraph = document.getElementById('joke');
@@ -8,7 +8,7 @@ var paragraph = document.getElementById('joke');
 function getJoke() {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', url);
-  xhr.addEventListener('load', function(){
+  xhr.addEventListener('load', function() {
     var response = JSON.parse(xhr.response);
         paragraph.innerHTML = response.value.joke;
   });
